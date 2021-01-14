7d14841598c08c234877d0af586dfe8b0758a82e
goreyr <rcgorey@gmail.com>
2021-01-13 23:52:10 -0800

    Added start of a Card and Deck class, implemented deck shuffle

---
+++ Assets/Scenes/Card.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum _suit
+    {
+        Chest,
+        Cutlass,
+        Parrot,
+        Ship,
+    }
+
+    private int pointValue
+    {
+        get { return pointValue; }
+        set { pointValue = value; }
+    }
+    private string displayName
+    {
+        get { return displayName;}
+        set { displayName = value}
+    }
+    // need to figure out how to make a sprite property, and what to do with the sprite sheet
+    // private sprite cardFace
+    // private sprite cardBack
+    private _suit suit
+    {
+        get { return suit; }
+        set { suit = value; }
+    }
+
+    public Card(int pointValue, string displayName, _suit suit)
+    {
+        this.pointValue = pointValue;
+        this.displayName = displayName;
+        this.suit = suit;
+        // set card front and back sprites
+    }
+
+    public Card(int pointValue, _suit suit)
+    {
+        this.pointValue = pointValue;
+        this.displayName = pointValue.ToString();
+        this.suit = suit;
+        // set card front and back sprites
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scenes/Deck.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Card;
+using System.Random;
+
+public class Deck : MonoBehaviour
+{
+    private ArrayList<Card> cards;
+    // need to figure out how to make a sprite property, and what to do with the sprite sheet
+    // private sprite nonEmptyDeck;
+    // private sprite emptyDeck;
+    
+    public Deck(int numStandardDecks = 1)
+    {
+        ArrayList<Card> myCards = new ArrayList<Card>();
+
+        for (int i = 1; i <= 13; i++)
+        {
+            // foreach(_suit suit in enum.getValues(typeOf(_suit))) Q: Where should I be storing the enum value? Presumably its own C# file since I want to reference it outside of Card.cs?
+            for (int j = 0; j <= 3; j++)
+            {
+                curCard = new Card(i, (_suit)j);
+                myCards.Add(curCard);
+            }
+        }
+
+        this.cards = myCards;
+        this.Shuffle();
+    }
+
+    public Deck(ArrayList<Card> cards)
+    {
+        this.cards = cards;
+        this.Shuffle()
+    }
+
+    public Shuffle()
+    {
+        // Fisher-Yates & Durstenfield algorithm
+        // swap the ith 
+        rnd = new Random();
+        int j;
+        var tempCard = new Card(); 
+        
+        for (int i = 0; i <= cards.Count - 2; i++)
+        {
+            j = rnd.Next(i, cards.Count);
+            tempCard = cards[i];
+            cards[i] = cards[j];
+            cards[j] = tempCard;
+        } 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
