b722e991f33ca94f36ef8168a40cbd88733c1242
tristanjohnson849 <75589505+tristanjohnson849@users.noreply.github.com>
2021-02-05 14:20:55 -0800

    Add Bank/Bet demo UI

---
+++ Assets/Bet10Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Bet10Button : MonoBehaviour, IPointerClickHandler
+{
+    public Player player;
+
+    public void OnPointerClick(PointerEventData eventData) => player.Raise(10);
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text bankDisplay;
+    public int Bank
+    { get; set; }
+
+    public Text betDisplay;
+    public int Bet
+    { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bank = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bankDisplay.text = $"Bank: {Bank}";
+        betDisplay.text = $"Bet: {Bet}";
+    }
+
+    public void Raise(int value)
+    {
+        if (Bank - value >= 0)
+        {
+            Bank -= value;
+            Bet += value;
+        }
+    }
+}
